05437800a60f2d65737ee698f6cb6b8202f4efa8
PhamSonUIT <thanhsonpham180803@gmail.com>
2024-05-14 11:19:08 +0700

    urls.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import NhanVien
 
 # Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
