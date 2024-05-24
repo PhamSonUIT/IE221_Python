748115110e08e1f8827d6887214a2e3002896966
PhamSonUIT <thanhsonpham180803@gmail.com>
2024-05-24 11:25:21 +0700

    html

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', )
+    path('', views.home,name="home"),
+    path('hien_thi', views.hien_thi,name="show"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,12 @@ from .models import NhanVien
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+def hien_thi(request):
+    ds_nv = NhanVien.objects.all()
+    return render(request, 'app/hien_thi_nv.html', {'nv':ds_nv})
+
+def liet_ke_nv(requets):
+    ds_nv = NhanVien.objects.all()
+    output = "</br>".join([nv.ho_ten for nv in ds_nv])
+    return HttpResponse(output)
