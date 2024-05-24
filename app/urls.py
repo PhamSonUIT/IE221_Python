aa49b9c7a2ee7e2962976ff68da5c407bdb4e692
PhamSonUIT <thanhsonpham180803@gmail.com>
2024-05-24 15:03:27 +0700

    them_nv

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home,name="home"),
-    path('hien_thi', views.hien_thi,name="show"),
+    path('hien_thi_nv/', views.hien_thi_nv,name="hien_thi_nv"),
+    path('them_nv/', views.them_nv,name="them_nv"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import NhanVien
 
@@ -6,11 +6,38 @@ from .models import NhanVien
 def home(request):
     return render(request, 'app/home.html')
 
-def hien_thi(request):
+def hien_thi_nv(request):
     ds_nv = NhanVien.objects.all()
-    return render(request, 'app/hien_thi_nv.html', {'nv':ds_nv})
+    return render(request, 'app/hien_thi_nv.html', {'ds_nv':ds_nv})
 
-def liet_ke_nv(requets):
-    ds_nv = NhanVien.objects.all()
-    output = "</br>".join([nv.ho_ten for nv in ds_nv])
-    return HttpResponse(output)
+def tim_nv(request, ma_nv):
+    return render(request, 'app/tim_nv.html',{'ma_nv':ma_nv} )
+
+
+def them_nv(request):
+    if request.method == 'POST':
+        ma_nv = request.POST['ma_nv']
+        ho_ten = request.POST['ho_ten']
+        luong_cb = request.POST['luong_cb']
+        loai_nv = request.POST['loai_nv']
+        
+        so_gio_lam = request.POST.get('so_gio_lam', 0)
+        so_sp = request.POST.get('so_sp', 0)
+        he_so_cv = request.POST.get('he_so_cv', 0)
+        thuong = request.POST.get('thuong', 0)
+        
+        nhan_vien = NhanVien(
+            ma_nv=ma_nv, 
+            ho_ten=ho_ten, 
+            luong_cb=luong_cb, 
+            loai_nv=loai_nv, 
+            so_gio_lam=so_gio_lam, 
+            so_sp=so_sp, 
+            he_so_cv=he_so_cv, 
+            thuong=thuong
+        )
+        nhan_vien.save()
+        
+        return redirect('hien_thi_nv')
+    return render(request, 'app/them_nv.html')
+
